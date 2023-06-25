ce9d972e0250ab0b1032d6eca29c177c399aae3e
Dmitry Shakhin <dshahin@yandex.ru>
2023-06-25 18:47:01 +0300

    Сравнение трех чисел

---
+++ Task 4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел
+
+System.Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+
+System.Console.WriteLine($"{max} - максимальное число");
