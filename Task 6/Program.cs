97ad3a6e83edb67595c90711799791b987a0bf6f
Dmitry Shakhin <dshahin@yandex.ru>
2023-06-25 18:55:42 +0300

    Число четное или нечетное

---
+++ Task 6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число четным
+
+System.Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    System.Console.WriteLine($"Число {a} - четное");
+}
+else
+{
+    System.Console.WriteLine($"Число {a} - нечетное");
+}
