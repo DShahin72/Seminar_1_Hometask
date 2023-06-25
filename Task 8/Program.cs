eb38565af56052d15ef4d07beec0caf54dc12016
Dmitry Shakhin <dshahin@yandex.ru>
2023-06-25 19:20:07 +0300

    Вывод четных чисел от 1 до N

---
+++ Task 8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число N, а на выходе показывает все четные числа от 1 до N
+
+System.Console.Write("Введите натуральное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 1)
+{
+    System.Console.WriteLine("Вы ввели ненатуральное число!");
+}
+
+for (int i = 2; i <= N; i += 2)
+{
+    System.Console.Write(i + " ");
+}
