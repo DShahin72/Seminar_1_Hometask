2f344abe4d418fad3fddeefd225a3c1ddd9e1a0f
Dmitry Shakhin <dshahin@yandex.ru>
2023-06-25 18:35:29 +0300

    Сравнение двух чисел

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число больше, а какое меньше
+
+System.Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    System.Console.WriteLine($"Число {a} больше числа {b}");
+}
+else if (a < b)
+{
+    System.Console.WriteLine($"Число {b} больше числа {a}");
+}
+else
+{
+    System.Console.WriteLine($"Число {a} равно числу {b}");
+}
